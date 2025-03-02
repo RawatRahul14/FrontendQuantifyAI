1c9db3291663f275bb0b27f2928bd9e35a22fc47
RawatRahul14 <rawatrahul1604@gmail.com>
2025-03-02 18:18:43 -0500

    overview

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Hero from "./components/Hero";
 import Brand from "./components/Brand";
 import Feature from "./components/Feature";
 import Process from "./components/Process";
+import Overview from "./components/Overview";
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
         <Brand />
         <Feature />
         <Process />
+        <Overview />
       </main>
     </div>
     </ReactLenis>
