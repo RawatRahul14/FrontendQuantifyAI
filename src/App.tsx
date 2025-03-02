5ba91624f49c4c20ea83e7706c33dcc8a5b87e51
RawatRahul14 <rawatrahul1604@gmail.com>
2025-03-02 14:32:20 -0500

    brands

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { ReactLenis } from "lenis/react";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Brand from "./components/Brand";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
 
       <main>
         <Hero />
+        <Brand />
       </main>
     </div>
     </ReactLenis>
